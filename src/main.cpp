995265e4773a427e0ccfba83cba7bfcb9383c5c0
Diego Estrada <diegoestrada@MacBook-Air-de-Diego.local>
2022-11-11 14:59:11 -0600

    paddle movement working quite alright

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "Game/Game.h"
+#include "Game/Game.cpp"
+//Screen dimension constants
+
+Game *game = NULL;
+
+int main( int argc, char* args[] )
+{
+    game = new Game();
+
+    game->init("PONG ECS");
+    game->setup();
+
+    while (true && game->running())
+    {
+      game->frameStart();
+      game->handleEvents();
+      game->update();
+      game->render();
+      game->frameEnd();
+    }
+
+    game->clean();
+
+    return 0;
+}
